f25283aca1bb8d388426c5a1da9fb2ac139158c0
manojgain1988 <manojgain1988@gmail.com>
2024-01-16 11:51:17 +0600

    first commit

---
+++ psd-to-html - Copy/js/apps.js
@@ -0,0 +1,8 @@
+function bars(){
+
+    var bars = document.querySelector("ul");
+    bars.classList.toggle("active");
+
+    var bars = document.querySelector("i");
+    bars.classList.toggle("fa-times");
+}
